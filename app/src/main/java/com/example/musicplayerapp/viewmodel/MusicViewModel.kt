fa42def51774df13aae77a7f9d24b3b77e360971
yehyun <into1023@naver.com>
2021-11-11 16:24:53 +0900

    Rxjava 추가

--- app/src/main/java/com/example/musicplayerapp/viewmodel/MusicViewModel.kt
+++ app/src/main/java/com/example/musicplayerapp/viewmodel/MusicViewModel.kt
@@ -8,6 +8,9 @@ import kotlinx.coroutines.launch
 class MusicViewModel() : ViewModel() {
     // TODO viewModel에서 데이터 업데이트 되도록 구현 필요
 
+    // Rxjava를 이해하기 쉽다!
+    // https://blog.yena.io/studynote/2020/10/11/Android-RxJava(1).html
+
     // https://kangmin1012.tistory.com/39 참고
     // https://gus0000123.medium.com/android-jetpack-livedata-%EC%82%AC%EC%9A%A9%EB%B2%95-2-%EC%82%AC%EC%9A%A9%ED%8E%B8-890de112b4f4
     // https://hanyeop.tistory.com/198
